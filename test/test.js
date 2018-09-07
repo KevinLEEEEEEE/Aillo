80651fc6422af628e61af54b7d18de69c01c068e
Kevin <1069440019@qq.com>
2018-09-07 09:34:50 +0800

    fuction and UI test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,20 @@
+const Nightmare = require('nightmare');
 const assert = require('assert');
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+describe('imageDisplayAndConvert', () => {
+  describe('imageDisplay', () => {
+    it('unsupported format should be rejected', () => {
+      const nightmare = Nightmare();
+
+      nightmare
+        .goto('../dist/index.html')
+        .type('#rename', 'testName')
+        // .evaluate(() => document.getElementById('rename').value)
+        .then((value) => {
+          // console.log(document.getElementById('rename').value);
+          // assert.equal(value, 'testName');
+        });
+      // assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });

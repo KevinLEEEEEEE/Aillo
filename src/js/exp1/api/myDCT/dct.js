cb92e5238acc31ef2572fae6c300b2397c9a4fb0
Kevin <1069440019@qq.com>
2018-09-16 10:41:22 +0800

    1D dct

--- src/js/exp1/api/myDCT/dct.js
+++ src/js/exp1/api/myDCT/dct.js
@@ -1,16 +1,46 @@
 
 const _ = {
+  cu(u, N) {
+    return u === 0 ? Math.sqrt(1 / N) : Math.sqrt(2 / N);
+  },
+
   factorLut(len) {
     if (!Reflect.has(this, `factorLut${len}`)) {
+      const tmp = [];
 
+      for (let u = 0; u < len; u += 1) {
+        for (let i = 0; i < len; i += 1) {
+          const value = Math.cos((i + 0.5) * Math.PI * u / len);
+
+          tmp.push(value);
+        }
+      }
+
+      this[`factorLut${len}`] = tmp;
     }
 
     return this[`factorLut${len}`];
   },
+
+  cal(array) {
+    const { length } = array;
+    const factorLut = _.factorLut(length);
+
+    const result = array.map((value, u, arr) => {
+      const cu = _.cu(u, length);
+      const sum = arr.reduce((prev, val, j) => prev + val * factorLut[u * length + j]);
+
+      return cu * sum;
+    });
+
+    return result;
+  },
 };
 
 const dct = (array) => {
-  const factorLut = _.factorLut();
+  const i = _.cal(array);
+  // console.log(i);
+  return i;
 };
 
 const idct = (array) => {
--- src/js/exp1/discreteCosineTransform.js
+++ src/js/exp1/discreteCosineTransform.js
@@ -1,5 +1,10 @@
 import dcts from './api/myDCT/dct';
+import dct2s from './api/myDCT/dct2';
 
 export default function discreteCosineTransform() {
   dcts.dct([0, 0, 2, 2, 2, 2, 0, 0]);
+  dct2s.dct2([61, 19, 50, 20,
+    82, 26, 61, 45,
+    89, 90, 82, 43,
+    93, 59, 53, 97], 4, 4);
 }

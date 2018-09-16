b16cbd20a9bb74f70c0e8420379a768dccce3062
Kevin <1069440019@qq.com>
2018-09-16 22:19:22 +0800

    dct 2D done!

--- src/js/exp1/api/myDCT/dct.js
+++ src/js/exp1/api/myDCT/dct.js
@@ -4,6 +4,15 @@ const _ = {
     return u === 0 ? Math.sqrt(1 / N) : Math.sqrt(2 / N);
   },
 
+  cu2D(u, v) {
+    if (u === 0 && v === 0) {
+      return 1 / 2;
+    } if (u === 0 || v === 0) {
+      return 1 / Math.sqrt(2);
+    }
+    return 1;
+  },
+
   factorLut(len) {
     if (!Reflect.has(this, `factorLut${len}`)) {
       const tmp = [];
@@ -22,6 +31,19 @@ const _ = {
     return this[`factorLut${len}`];
   },
 
+  factorLut2D(m, n, x, y, u, v) {
+    const factor = (a, b, c) => Math.cos((a + 0.5) * b * Math.PI / c);
+
+    return factor(x, u, m) * factor(y, v, n);
+  },
+
+  pos2D(index, w) {
+    const x = index % w;
+    const y = Math.floor(index / w);
+
+    return { x, y };
+  },
+
   cal(array) {
     const { length } = array;
     const factorLut = _.factorLut(length);
@@ -35,12 +57,36 @@ const _ = {
 
     return result;
   },
+
+  cal2D(array, w, h) {
+    const factor = 2 / Math.sqrt(w * h);
+
+    return array.map((val, i) => {
+      const { x: u, y: v } = _.pos2D(i, w);
+      const reducer = (prev, currentValue, index) => {
+        const { x, y } = _.pos2D(index, w);
+
+        return prev + currentValue * _.cu2D(u, v) * _.factorLut2D(w, h, x, y, u, v);
+      };
+
+      return factor * array.reduce(reducer, 0);
+    });
+  },
+
+  cal2D88(array) {
+    return _.cal2D(array, 8, 8);
+  },
 };
 
-const dct = (array) => {
-  const i = _.cal(array);
-  // console.log(i);
-  return i;
+const dct = (array, w, h) => {
+  // const i = _.cal(array);
+
+  // return i;
+
+  const res = _.cal2D(array, w, h);
+  console.log(res);
+
+  return res;
 };
 
 const idct = (array) => {
--- src/js/exp1/discreteCosineTransform.js
+++ src/js/exp1/discreteCosineTransform.js
@@ -2,9 +2,11 @@ import dcts from './api/myDCT/dct';
 import dct2s from './api/myDCT/dct2';
 
 export default function discreteCosineTransform() {
-  dcts.dct([0, 0, 2, 2, 2, 2, 0, 0]);
-  dct2s.dct2([61, 19, 50, 20,
-    82, 26, 61, 45,
-    89, 90, 82, 43,
-    93, 59, 53, 97], 4, 4);
+  // console.log(dcts.dct([0, 0, 2, 2, 2, 2, 0, 0]));
+  dcts.dct([42, 66, 68, 66,
+    92, 4, 76, 17,
+    79, 85, 74, 71,
+    96, 93, 39, 3], 4, 4);
+
+  // dcts.dct()
 }
